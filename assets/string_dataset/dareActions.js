53d7efb2032dec47f566e9f7ac7198f8629bff62
Ferdy Hahan Pradana <75787853+ferdyhape@users.noreply.github.com>
2023-06-13 03:17:14 +0700

    add new assets

---
+++ assets/string_dataset/dareActions.js
@@ -0,0 +1,6 @@
+const dareActions = [
+    "Capture dan kirim baris chat Whatsapp teratas kamu",
+    "Capture dan kirim baris chat direct message Instagram teratas kamu",
+];
+
+export default dareActions;
---
+++ assets/string_dataset/truthQuestions.js
@@ -0,0 +1,6 @@
+const truthQuestions = [
+    "Apa kamu pernah selingkuh?",
+    "Apa kamu selingkuh sekarang?",
+]
+
+export default truthQuestions;
