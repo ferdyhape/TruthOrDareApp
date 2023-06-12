2bfc8122a093a6634b0480a2f27c78dacb33a7cc
Ferdy Hahan Pradana <75787853+ferdyhape@users.noreply.github.com>
2023-06-12 15:59:58 +0700

    add sidebar

--- main.py
+++ main.py
@@ -1,6 +1,110 @@
 import streamlit as st
 import random
 
+# set page config
+st.set_page_config(
+    page_title="TOD Game App | FERDYHAPE", 
+    page_icon=":gear:",
+    layout="centered", 
+    initial_sidebar_state="auto", menu_items=None
+    )
+
+# sidebar for author introduction
+st.sidebar.markdown("""
+
+<head>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.1/css/all.min.css"
+        integrity="sha512-MV7K8+y+gLIBoVD59lQIYicR65iaqukzvf/nwasF0nqhPay5w/9lJmVM2hMDcnK1OnMGCdVK+iQrJ7lzPJQd1w=="
+        crossorigin="anonymous" referrerpolicy="no-referrer" />
+</head>
+
+<body>
+    <div class="profile_picture">
+        <img src="https://github.com/ferdyhape.png" alt="Profile_Picture" srcset="">
+    </div>
+    <p class ="html" style='text-align: center;'>Connect with me!</p>
+    <div class="group-icon">
+        <a href="https://github.com/ferdyhape" target="_blank"><i class="fa-brands fa-github"></i></a>
+        <a href="https://instagram.com/ferdyhape" target="_blank"><i class="fa-brands fa-instagram"></i></a>
+        <a href="https://www.linkedin.com/in/ferdy-hahan-pradana/" target="_blank"><i class="fa-brands fa-linkedin"></i></a>
+    </div> 
+    <footer>
+        <p class="copyright">©2023<br> Copyright By <a href="https://github.com/ferdyhape">FERDYHAPE</a></p>
+    </footer>
+    <style>
+    .profile_picture {
+        text-align:center;
+    }
+    .profile_picture img{
+      border-radius: 50%;
+      width: 65%;
+    }
+    .group-icon {
+        margin: 10px 20px;
+        padding: 0;
+        border-radius: 25px;
+        background-color: #F6F6F6;
+        text-align: center;
+        font-size: 25px;
+    } 
+    .group-icon:hover {
+        background-color: #F9F9F9;
+        cursor: pointer;
+    }
+    .fa-github {
+        color: #333;
+    }
+    .fa-instagram {
+        color: #833AB4;
+    }
+    .fa-linkedin {
+        color: #0e76a8;
+    }
+    .author {
+        margin: 0px 10px;;
+        font-size: 20px;
+        font-weight: bold;
+    }
+    .html {
+        margin: 10px 10px;
+        padding: 0;
+    }
+    footer {
+        position: static;
+        height: 280px;
+        width: 100%;
+    }
+    .copyright{
+        position: absolute;
+        width: 100%;
+        color: #fff;
+        line-height: 20px;
+        font-size: 1em;
+        text-align: center;
+        bottom:0;
+    }
+    .copyright a {
+        margin: 0;
+        text-decoration: none;
+    }
+    footer p {
+        margin: 0;
+    }
+    a {
+        font-weight: bolder;
+        margin: 0 10px;
+    }
+    </style>
+</body>
+
+""", unsafe_allow_html=True)
+
+# set the title of page
+def title_template(title):
+    st.title(f"""
+    {title}
+    """)
+
 truth_questions = [
     'Apa kamu pernah selingkuh?',
     'Apa kamu selingkuh sekarang?',
@@ -98,7 +202,7 @@ dare_actions = [
 
 def main():
     st.title("Truth or Dare App")
-    st.write("Click the button below to get a random Truth or Dare question.")
+    st.write("Click the button below to get a random Truth and Dare question.")
     columns = st.columns(8)
 
     if columns[0].button("Truth"):
